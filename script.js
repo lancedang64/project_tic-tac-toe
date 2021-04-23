2e8102f9f3ae73af54236a2d017e2a7ecc639d73
Lance Dang <lancedang64@gmail.com>
2021-04-23 12:21:49 +1000

    Added several game mechanics
    Added logic to check if a move is valid
    Added logic to check if win condition is met
    Added logic to end the game if win condition is met or continue
    by switching player

--- script.js
+++ script.js
@@ -1,49 +1,116 @@
-'use strict'
+"use strict";
 
 const gameBoard = (() => {
-    const gameGrid = [];
-    for (let i = 0; i < 9; i++) gameGrid.push('');
+  const gameGridArray = [];
+  for (let i = 0; i < 9; i++) gameGridArray.push("");
 
-    let currentPlayer;
+  const xLocations = [];
+  const oLocations = [];
 
-    const startNewGame = () => {
-        currentPlayer = player1;
-    };
+  const winConditionArrays = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+  ];
 
-    const playMove = (e) => {
-        const squareSelected = e.target.id.slice(1);
-        gameGrid[squareSelected] = currentPlayer.symbol;
-        render();
-        switchCurrentPlayer()
-    };
-    
-    const render = () => {
-        for (let i = 0; i < 9; i++) {
-            const squareContent = document.getElementById(`c${i}`);
-            squareContent.textContent = gameGrid[i];
-        }        
-    };
+  let currentPlayer;
 
-    const switchCurrentPlayer = () => {
-        if (currentPlayer == player1) currentPlayer = player2;
-        else currentPlayer = player1;
-    };
+  const startNewGame = () => {
+    currentPlayer = player1;
+  };
 
-    return {startNewGame, playMove};
+  const playMove = (e) => {
+    const squareSelected = e.target.id.slice(1);
+    if (!isMoveValid(squareSelected)) return;
+    else {
+      updateSymbolLocation(squareSelected);
+      updateGridArray(squareSelected);
+      render(squareSelected);
+      if (isGameOver()) {
+        endGame(currentPlayer);
+        return;
+      } else switchCurrentPlayer();
+    }
+  };
+
+  const isMoveValid = (squareSelected) => {
+    if (gameGridArray[squareSelected] !== "") return false;
+    else return true;
+  };
+
+  const updateSymbolLocation = (squareSelected) => {
+    const indexSelected = parseInt(squareSelected);
+    if (currentPlayer == player1) xLocations.push(indexSelected);
+    else oLocations.push(indexSelected);
+  };
+
+  const updateGridArray = (squareSelected) => {
+    gameGridArray[squareSelected] = currentPlayer.symbol;
+  };
+
+  const render = (squareSelected) => {
+    const squareContent = document.getElementById(`c${squareSelected}`);
+    squareContent.textContent = gameGridArray[squareSelected];
+  };
+
+  const isGameOver = () => {
+    // check if any winConditionArray is subset of X or O arrays
+    console.log("checking if game is over");
+    let currentSymbolLocations;
+    if (currentPlayer == player1) currentSymbolLocations = xLocations;
+    else currentSymbolLocations = oLocations;
+    let isGameOver = false;
+
+    winConditionArrays.forEach(winCondition => {
+        if (isArraySubsetOfAnother(winCondition, currentSymbolLocations)) {
+            isGameOver = true;
+            return;    
+        }
+    });
+    return isGameOver;
+  };
+
+  const isArraySubsetOfAnother = (subsetArray, supersetArray) => {
+    for (let i = 0; i < subsetArray.length; i++) {
+        for (let j = 0; j < supersetArray.length; j++) {
+            if (subsetArray[i] == supersetArray[j]) {
+                break;
+            } else if (j == supersetArray.length - 1) return false;
+        }
+    }
+    return true;
+  };
+
+  const endGame = (currentPlayer) => {
+    // freeze display
+    console.log(`Game Over, ${currentPlayer.name} is the winner`);
+  };
+
+  const switchCurrentPlayer = () => {
+    if (currentPlayer == player1) currentPlayer = player2;
+    else currentPlayer = player1;
+  };
+
+  return { startNewGame, playMove, xLocations, oLocations, isArraySubsetOfAnother };
 })();
 
 const displayController = (() => {
-    const addPlayListeners = () => {
-        const gameSquares = document.querySelectorAll(".game-square");
-        gameSquares.forEach(square => {
-            square.addEventListener('click', gameBoard.playMove);
-        });
-    };
-    return {addPlayListeners};
+  const addPlayListeners = () => {
+    const gameSquares = document.querySelectorAll(".game-square");
+    gameSquares.forEach((square) => {
+      square.addEventListener("click", gameBoard.playMove);
+    });
+  };
+  return { addPlayListeners };
 })();
 
 const Player = (name, symbol) => {
-    return {name, symbol};
+  return { name, symbol };
 };
 
 const player1 = Player("Player 1", "X");
